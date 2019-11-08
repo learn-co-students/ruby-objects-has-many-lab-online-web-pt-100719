76d40a54aa0670a45becf1d5d2dead7a8c320d1e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-08 00:51:33 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def add_song_by_name(song_name)
+   art =  Song.new(song_name)
+    add_song(art)
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+  
+end
